01d98722ca7368928357fece489ce27dbac882a8
alokhegde2 <hegdealok@yahoo.com>
2021-06-05 21:18:38 +0530

    Completed registration of a user functionality

--- frontend/auth/js/register.js
+++ frontend/auth/js/register.js
@@ -1,74 +1,121 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-const login = document.getElementById('login')
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const login = document.getElementById("login");
+const error = document.getElementById("error")
 
-const isError = false;
-
+let isError = false;
 
 //Show input error message
-function showError(input,message){
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
 //Show success outline
 function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success'
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
 }
 
 //check required fields
 function checkRequired(inputArr) {
-    inputArr.forEach(input => {
-        if (input.value.trim() === "") {
-            showError(input,`${getFieldName(input)} is required`)
-        } else {
-            showSuccess(input)
-        }
-    });
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+      isError=true
+    } else {
+      showSuccess(input);
+      isError=false
+    }
+  });
 }
 
 //check input length
-function checkLength(input,min,max) {
-    if (input.value.length<min) {
-        showError(input,`${getFieldName(input)} must be atleast ${min} characters`);
-    } else if (input.value.length>max) {
-        showError(input,`${getFieldName(input)} must be less than ${max} characters`);
-    } else {
-        showSuccess(input)
-    }
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be atleast ${min} characters`
+    );
+    isError=true;
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+    isError=true;
+  } else {
+    showSuccess(input);
+    isError=false;
+  }
 }
 
 //Check password match
-function checkPasswordsMatch(input1,input2) {
-    if (input1.value !== input2.value ) {
-        showError(input2,'Passwords do not match')
-    } else {
-        showSuccess(input1)
-    }
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+    isError=true
+  } else {
+    showSuccess(input1);
+    isError=false
+  }
+}
+
+//POST request
+function doRequest(username,mail,pass) {
+    console.log(mail.value)
+  fetch("http://127.0.0.1:5000/user/register", {
+    method: "POST",
+    body: JSON.stringify({
+        email:mail.value,
+        name:username.value,
+        password:pass.value
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        error.innerText = "";
+        window.location.assign("../html/login.html");
+        return response.json();
+      }
+      if(response.status == 409){
+          console.log("Email is already registered")
+          error.innerText = "Email is already registered";
+      }
+      return Promise.reject(response);
+    })
+    .catch(function (error) {
+      console.warn("Something went wrong.", error);
+    });
 }
 
 //Get fieldname
 function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 //Event Listners
-form.addEventListener('submit',function(e) {
-    e.preventDefault();
-    checkRequired([username,email,password,password2]);
-    checkLength(username,3,15);
-    checkLength(password,6,15);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 15);
+  checkPasswordsMatch(password,password2)
+  if(!isError){
+  doRequest(username, email, password);
+  }
 });
 
-
-//on click on login 
-login.addEventListener('click',function(e) {
-    e.preventDefault();
-    window.location.assign("../html/login.html")
-})
+//on click on login
+login.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.assign("../html/login.html");
+});
