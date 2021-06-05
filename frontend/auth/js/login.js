3467f13c0d87bde78dc3ab507f2d72773d9e0361
alokhegde2 <hegdealok@yahoo.com>
2021-06-05 19:59:26 +0530

    Added login ui

--- frontend/auth/js/login.js
+++ frontend/auth/js/login.js
@@ -1,6 +1,46 @@
 const register = document.getElementById('register')
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
 
+
+//Show input error message
+function showError(input,message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message
+}
+
+//Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'
+}
+
+//check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(input => {
+        if (input.value.trim() === "") {
+            showError(input,`${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+    });
+}
+
+//Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+}
+
+//on click to submit
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+    checkRequired([email,password]);
+    console.log("Submited")
+});
 
 //on click on register 
 register.addEventListener('click',function(e) {
